ab59d604789753b4fe480e6fe32c4492ffd53a91
Ekaterina <cat.novokshenova666@yandex.ru>
2026-09-16 20:38:19 +0300

    Update MtsPage.java

--- src/main/java/org/example/MtsPage.java
+++ src/main/java/org/example/MtsPage.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-public class age {
+public class MtsPage {
 
     private final WebDriver driver;
     private final WebDriverWait wait;
